f712bee7304d6134e9f0fa135908131415d65cfa
unknown <alumno@ILERNA.SEVILLA>
2022-02-11 09:09:40 +0100

    Trabajo en la branch paralela

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+    function precio(){
+        var preciototal = 0.00;
+        var preciodatos = "";
+        var preciocontasion = "";
+      
+        $(".product").each(function(){
+          preciodatos = parseFloat($(this).find(".tp-sel > option:selected").data("price")) || 0;
+          preciocontasion = parseFloat($(this).find(".tp-sel-count > option:selected").data("count")) || 0;
+          console.log("Price: " + preciodatos + "Count:" +preciocontasion);
+          preciototal += preciodatos * preciodatos;
+        })
+        $("#price").html(preciototal.toFixed(2));
+      }
+      
+      
+      $("#countprice").click(function(){
+        precio();
+      });
+
+
 });
